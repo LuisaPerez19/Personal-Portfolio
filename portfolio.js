d2659261c6dc5eae9fe2e0d129537181c977375f
Luisa Perez <luisa.perezw3@gmail.com>
2023-09-25 20:03:51 +0100

    adding js backend for cards pop up individual information

--- portfolio.js
+++ portfolio.js
@@ -3,15 +3,31 @@
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("view-project-btn")) {
     togglePortfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
   }
 });
 
 function togglePortfolioPopup() {
   document.querySelector(".portfolio-popup").classList.toggle("open");
   document.body.classList.toggle("hide-scrolling");
-  document.querySelector("portfolio").classList.toggle("fade-out");
+  document.querySelector(".all-content").classList.toggle("fade-out");
 }
 
 document
   .querySelector(".pp-close")
   .addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+  console.log(portfolioItem);
+  document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(
+    ".portfolio-item-thumbnail img"
+  ).src;
+
+  document.querySelector(
+    ".pp-header h3"
+  ).innerHTML = portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+  document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(
+    ".portfolio-item-details"
+  ).innerHTML;
+}
